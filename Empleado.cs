8e879ae3d5e83d72de22b52d3f8a1a3f1993ddd4
martincabrera1993 <martincm22@uniandes.edu.ec>
2026-06-05 17:24:17 -0500

    Empleado.cs

---
+++ Empleado.cs
@@ -0,0 +1,11 @@
+namespace Diseño_Empleados.Views.Home
+{
+    public class Empleado
+    {
+        public int Empleado_Id;
+        public required string Nombre;
+        public required string Apellido;
+        public required string Email;
+        public required string Telefono;
+    }
+}
